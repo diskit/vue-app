618653ea0aab106a49c1efe6d1d43b06678ec875
diskit <diskit.1980@gmail.com>
2021-05-02 10:43:51 +0900

    add props

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <!-- <HelloWorld msg="Hello Vue 3.0 + Vite" /> -->
+  <Props msg="Props" :callback="callback"></Props>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script lang="ts">
+import { defineComponent, onMounted, reactive } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+import Props from './components/Props.vue';
 
 export default defineComponent({
   components: {
-    HelloWorld
+    HelloWorld,
+    Props
+  },
+  setup() {
+    const callback = (v: string) => {
+      console.log(v);
+    }
+    return {
+      callback
+    }
   }
 })
 </script>
---
+++ src/components/Props.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="increment()">count is: {{ count }}</button>
+</template>
+
+<script lang="ts">
+
+import { defineComponent, ref, PropType } from 'vue'
+
+export default defineComponent({
+  props: {
+    msg: String,
+    callback: {
+      type: Function as PropType<(v: String) => void>,
+      required: true
+    }
+  },
+  setup(props) {
+    const count = ref(0);
+    const increment = () => {
+      count.value++;
+      props.callback(`*** ${count.value} clicked ***`);
+    }
+    return {
+      count, increment
+    }
+  }
+})
+</script>
